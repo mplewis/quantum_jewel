5f93ae2efa3e5cc73e5ab69e55a4f762607fb1b8
Matthew Lewis <matt@mplewis.com>
2013-04-22 15:25:17 -0500

    get t/l shaped matches works now, get matched gems returns just gems, not gemsets

--- board.py
+++ board.py
@@ -5,60 +5,128 @@ class Board:
         self.height = board_height
         self.width = board_width
         self.board = [[randint(0, num_gem_types - 1) for x in xrange(self.width)] for x in xrange(self.height)]
-    def get_triple_sets(self):
-        all_triple_sets = set()
+    def get_matched_gems(self):
+        '''Finds all gems that match and will be scored.'''
+        # gems_to_break holds all gems that have been matched and will be broken on board update
+        gems_to_break = set()
         # get all horizontal matches by iterating over x inside y
         for y_coord in xrange(0, self.height):
             for x_coord in xrange(0, self.width):
-                single_triple_set = set()
-                # set.update(iterable) adds the items in the iterable to
-                # the set, so we have to make an iterable tuple with one
-                # coord pair to add to the set
-                single_triple_set.update( ((x_coord, y_coord), ) )
-                # adjacent_match_length is the number of same gems adjacent to one another
+                # single_triple_list holds the gem (x, y) and same-colored gems that are adjacent
+                single_triple_list = []
+                single_point_coord = (x_coord, y_coord)
+                single_triple_list.append(single_point_coord)
+                # adjacent_match_length is the number of same-colored gems adjacent to one another
                 adjacent_match_length = 1
                 # iterate through the next gems in the current y-row
                 for next_x_coord in xrange(x_coord + 1, self.width):
                     # get the gem values off the board to see if they're identical
                     gem_1 = self.board[y_coord][x_coord]
                     gem_2 = self.board[y_coord][next_x_coord]
-                    # check if gem 1 matches the gem to the right of this gem (gem 2)
+                    # check if gem 1 matches the gem to the right (gem 2)
                     if gem_1 == gem_2:
-                        # gems match, add the coord to the triple set
-                        adjacent_match_length += 1
+                        # gems match, add the coord to the triple set and increment the count
                         single_point_coord = (next_x_coord, y_coord)
-                        # again, create an iterable with one item, or
-                        # the set will iterate over the single point
-                        single_triple_set.update( (single_point_coord, ) )
+                        single_triple_list.append(single_point_coord)
+                        adjacent_match_length += 1
                     else:
                         # gems don't match, break out of the adjacent-check loop
                         break
                 # if there are 3 or more identical gems adjacent to one another...
                 if adjacent_match_length >= 3:
-                    # again, create an iterable with one set, or all_triple_sets
-                    # will try to iterate over all items in the set.
-                    # we must use frozenset because sets are mutable and the set
-                    # all_triple_sets will refuse to contain a mutable;
-                    # frozenset is not mutable and can be hashed into a set
-                    all_triple_sets.update((frozenset(single_triple_set), ))
-        # filter duplicate subsets of larger supersets (4-longs containing 3-longs)
-        num_triple_sets = len(all_triple_sets)
-        # we turn all_triple_sets into a list so we can reliably iterate over its
-        # items in a known order so that we don't compare one set to itself
-        all_triple_sets_list = list(all_triple_sets)
-        for point_1 in xrange(0, num_triple_sets):
-            for point_2 in xrange(point_1 + 1, num_triple_sets):
-                set_points_1 = all_triple_sets_list[point_1]
-                set_points_2 = all_triple_sets_list[point_2]
-                if set_points_1 >= set_points_2:
-                    # as usual, use a tuple to foil the iteration that set likes to do
-                    # set_points_1 is a proper superset of set_points_2, so remove set_points_2
-                    all_triple_sets.difference_update( (set_points_2, ) )
-                elif set_points_2 >= set_points_1:
-                    # set_points_2 is a proper superset of set_points_1, so remove set_points_1
-                    all_triple_sets.difference_update( (set_points_1, ) )
-        return all_triple_sets
+                    # ...add all gem coords to the set of all gems to break
+                    gems_to_break.update(single_triple_list)
+        # get all vertical matches by iterating over y inside x
+        for x_coord in xrange(0, self.height):
+            for y_coord in xrange(0, self.width):
+                # single_triple_list holds the gem (x, y) and same-colored gems that are adjacent
+                single_triple_list = []
+                single_point_coord = (x_coord, y_coord)
+                single_triple_list.append(single_point_coord)
+                # adjacent_match_length is the number of same-colored gems adjacent to one another
+                adjacent_match_length = 1
+                # iterate through the next gems in the current y-row
+                for next_y_coord in xrange(y_coord + 1, self.width):
+                    # get the gem values off the board to see if they're identical
+                    gem_1 = self.board[y_coord][x_coord]
+                    gem_2 = self.board[next_y_coord][x_coord]
+                    # check if gem 1 matches the gem below (gem 2)
+                    if gem_1 == gem_2:
+                        # gems match, add the coord to the triple set and increment the count
+                        single_point_coord = (x_coord, next_y_coord)
+                        single_triple_list.append(single_point_coord)
+                        adjacent_match_length += 1
+                    else:
+                        # gems don't match, break out of the adjacent-check loop
+                        break
+                # if there are 3 or more identical gems adjacent to one another...
+                if adjacent_match_length >= 3:
+                    # ...add all gem coords to the set of all gems to break
+                    gems_to_break.update(single_triple_list)
+        return gems_to_break
+    def __get_gems_with_bounds(self):
+        t_match_bases = set()
+        gem_list = []
+        for gem_coord in self.get_matched_gems():
+            x_base, y_base = gem_coord
+            lower_bound_x = x_base - 2
+            upper_bound_x = x_base + 2
+            lower_bound_y = y_base - 2
+            upper_bound_y = y_base + 2
+            if lower_bound_x < 0:
+                lower_bound_x = 0
+            if upper_bound_x > self.width - 1:
+                upper_bound_x = self.width - 1
+            if lower_bound_y < 0:
+                lower_bound_y = 0
+            if upper_bound_y > self.height - 1:
+                upper_bound_y = self.height - 1
+            gem_data = {"gem_coord": gem_coord,
+                        "lower_bound_x": lower_bound_x,
+                        "upper_bound_x": upper_bound_x,
+                        "lower_bound_y": lower_bound_y,
+                        "upper_bound_y": upper_bound_y}
+            gem_list.append(gem_data)
+        return gem_list
+    def get_matches_t_l(self):
+        '''Finds all T-shaped and L-shaped matches.'''
+        gems_and_bounds = self.__get_gems_with_bounds()
+        l_bases = set()
+        for gem_data in gems_and_bounds:
+            base_gem_x, base_gem_y = gem_data["gem_coord"]
+            this_gem_color = self.board[base_gem_y][base_gem_x]
+            first_adj_gem_found = False
+            adjacent_match_length = 0
+            for x_coord in xrange(gem_data["lower_bound_x"], gem_data["upper_bound_x"] + 1):
+                next_gem_color = self.board[base_gem_y][x_coord]
+                if not first_adj_gem_found:
+                    if this_gem_color == next_gem_color:
+                        first_adj_gem_found = True
+                        adjacent_match_length += 1
+                else:
+                    if this_gem_color == next_gem_color:
+                        adjacent_match_length += 1
+                    else:
+                        break
+            if adjacent_match_length >= 3:
+                first_adj_gem_found = False
+                adjacent_match_length = 0
+                for y_coord in xrange(gem_data["lower_bound_y"], gem_data["upper_bound_y"] + 1):
+                    next_gem_color = self.board[y_coord][base_gem_x]
+                    if not first_adj_gem_found:
+                        if this_gem_color == next_gem_color:
+                            first_adj_gem_found = True
+                            adjacent_match_length += 1
+                    else:
+                        if this_gem_color == next_gem_color:
+                            adjacent_match_length += 1
+                        else:
+                            break
+                if adjacent_match_length >= 3:
+                    print (base_gem_x, base_gem_y)
     def get_board(self):
+        '''Returns a 2D matrix of the gems on the board.'''
         return self.board
     def set_board(self, to_set):
+        '''Sets the board to an arbitrary 2D matrix of gems.'''
         self.board = to_set
--- demo.py
+++ demo.py
@@ -1,47 +1,67 @@
 from pprint import pprint
 from board import Board
 
-use_superset_board = False
+use_superset_board  = False
+use_one_match_board = False
+use_one_t_board     = True
 
-superset_board = ([[7, 4, 0, 5, 3, 4, 3, 0],
-                   [4, 5, 3, 5, 7, 7, 7, 7],
-                   [5, 1, 6, 0, 2, 0, 1, 4],
-                   [5, 4, 6, 3, 0, 1, 1, 1],
-                   [1, 4, 4, 7, 0, 6, 2, 5],
-                   [4, 0, 4, 4, 4, 7, 7, 1],
-                   [3, 2, 3, 4, 2, 2, 5, 1],
-                   [5, 5, 3, 3, 2, 4, 0, 7]])
+superset_board  = ([[7, 4, 0, 5, 3, 4, 3, 0],
+                    [4, 5, 3, 5, 7, 7, 7, 7],
+                    [5, 1, 6, 0, 2, 0, 1, 4],
+                    [5, 4, 6, 3, 0, 1, 1, 1],
+                    [1, 4, 4, 7, 0, 6, 2, 5],
+                    [4, 0, 4, 4, 4, 7, 7, 1],
+                    [3, 2, 3, 4, 2, 2, 5, 1],
+                    [5, 5, 3, 3, 2, 4, 0, 7]])
 
+one_match_board = ([[3, 4, 1, 3, 5, 2, 1, 6],
+                    [0, 1, 4, 7, 4, 7, 5, 5],
+                    [5, 2, 1, 0, 5, 5, 5, 1],
+                    [4, 2, 2, 4, 2, 3, 0, 6],
+                    [4, 1, 3, 7, 1, 5, 7, 0],
+                    [1, 5, 0, 1, 3, 4, 4, 2],
+                    [5, 0, 3, 5, 1, 5, 6, 0],
+                    [6, 7, 0, 0, 2, 6, 6, 3]])
 
+one_t_board     = ([[3, 4, 1, 3, 5, 2, 1, 6],
+                    [0, 1, 4, 7, 4, 7, 5, 5],
+                    [5, 2, 1, 0, 5, 5, 5, 1],
+                    [4, 2, 2, 4, 2, 5, 0, 6],
+                    [4, 1, 3, 7, 1, 5, 7, 0],
+                    [1, 5, 0, 1, 3, 4, 4, 2],
+                    [5, 0, 3, 5, 1, 5, 6, 0],
+                    [6, 7, 0, 0, 2, 6, 6, 3]])
 
-if not use_superset_board:
-    # create board with default size 8x8
-    num_triples = 0
-    while num_triples < 3:
-        bd = Board()
-        all_triple_sets = bd.get_triple_sets()
-        num_triples = len(all_triple_sets)
-    pprint(all_triple_sets)
-else:
+if use_superset_board:
     bd = Board()
     bd.set_board(superset_board)
-    all_triple_sets = bd.get_triple_sets()
+    matched_gems = bd.get_matched_gems()
+elif use_one_match_board:
+    bd = Board()
+    bd.set_board(one_match_board)
+    matched_gems = bd.get_matched_gems()
+elif use_one_t_board:
+    bd = Board()
+    bd.set_board(one_t_board)
+    matched_gems = bd.get_matched_gems()
+else:
+    # create board with default size 8x8
+    num_gems_matched = 0
+    while num_gems_matched < 20:
+        bd = Board()
+        matched_gems = bd.get_matched_gems()
+        num_gems_matched = len(matched_gems)
+    pprint(matched_gems)
 
 raw_board = bd.get_board()
 pprint(raw_board)
 sparse_board = [[' ' for x in xrange(8)] for x in xrange(8)]
-pprint(all_triple_sets)
+pprint(matched_gems)
 
-# de-dupe all coordinates
-all_triple_coord_pairs = set()
-for triple_set in all_triple_sets:
-    for coord_pair in triple_set:
-        all_triple_coord_pairs.update( (coord_pair, ) )
-
-pprint(all_triple_coord_pairs)
-
-for coord_pair in all_triple_coord_pairs:
+for coord_pair in matched_gems:
     x_coord, y_coord = coord_pair
     sparse_board[y_coord][x_coord] = str(raw_board[y_coord][x_coord])
 
 pprint(sparse_board)
+
+bd.get_matches_t_l()
